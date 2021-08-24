28c962f2e1939bf822d22cea1ee3ea3dfed60c05
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2021-08-24 02:48:57 +0200

    signalbongs i think

--- Makefile
+++ Makefile
@@ -1,35 +1,18 @@
-BIN_NAME = utest
-
-#CC = x86_64-pc-aex2-gcc
-AS = nasm
-LD = ld
-
-MKDIR = mkdir -p
+BIN_NAME := utest
 
 BIN      := /tmp/aex2/bin/$(BIN_NAME)/
+BIN_OBJ  := $(BIN)$(BIN_NAME)
 DEP_DEST := $(BIN)dep/
 OBJ_DEST := $(BIN)obj/
 
-CFILES    := $(shell find . -type f -name '*.c')
-HFILES    := $(shell find . -type f -name '*.h')
-ASMFILES  := $(shell find . -type f -name '*.asm')
-OBJS      := $(patsubst %.o, $(OBJ_DEST)%.o, $(CFILES:.c=.c.o) $(ASMFILES:.asm=.asm.o))
+CFILES   := $(shell find . -type f -name '*.c')
+HFILES   := $(shell find . -type f -name '*.h')
+OBJS     := $(patsubst %.o, $(OBJ_DEST)%.o, $(CFILES:.c=.c.o))
 
-BIN_OBJ = $(BIN)$(BIN_NAME)
-
-GFLAGS = -O3 -pipe -flto
-
+CFLAGS   := -O3 -pipe -flto
 INCLUDES := -I. -Iinclude/
 
-CFLAGS := $(GFLAGS) \
-	$(INCLUDES)
-
-ASFLAGS := -felf64
-
-LDFLAGS := $(GFLAGS)        \
-	-ffreestanding          \
-	-z max-page-size=0x1000 \
-	-no-pie
+MKDIR := mkdir -p
 
 format:
 	@$(MKDIR) $(BIN)
@@ -37,9 +20,8 @@ format:
 
 all: $(OBJS)
 	@$(MKDIR) $(BIN)
-	$(CC) $(CFLAGS) -o $(BIN_OBJ) $(OBJS)
-
-include $(shell find $(DEP_DEST) -type f -name *.d)
+	@$(CC) $(CFLAGS) -o $(BIN_OBJ) $(OBJS)
+	@printf '\033[0;92m$(BIN_NAME)\033[0m: Done building\033[0K\n'
 
 copy:
 	@cp -u $(BIN_OBJ) "$(COPY_DIR)"
@@ -47,11 +29,10 @@ copy:
 clean:
 	rm -rf $(BIN)
 
+include $(shell find $(DEP_DEST) -type f -name *.d)
 $(OBJ_DEST)%.c.o : %.c
 	@$(MKDIR) ${@D}
 	@$(MKDIR) $(dir $(DEP_DEST)$*)
-	$(CC) $(CFLAGS) -c $< -o $@ -MMD -MT $@ -MF $(DEP_DEST)$*.cpp.d
 
-$(OBJ_DEST)%.asm.o : %.asm
-	@$(MKDIR) ${@D}
-	$(AS) $(ASFLAGS) $< -o $@
+	@printf '\033[0;92m$(BIN_NAME)\033[0m: Building \033[0;92m$(<)\033[0m\033[0K\r'
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@ -MMD -MT $@ -MF $(DEP_DEST)$*.c.d
--- main.c
+++ main.c
@@ -9,32 +9,39 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <time.h>
 #include <unistd.h>
 
-#define NOBONG
+// #define NOBONG
 
 #ifndef NOBONG
-#define ASSERT(condition)                                                                       \
-    ({                                                                                          \
-        printf("%s:%i: %s\n", __FILE__, __LINE__, #condition);                                  \
-                                                                                                \
-        if (!(condition)) {                                                                     \
-            fprintf(stderr, "%s:%i: Assertion failed! (%s)\n", __FILE__, __LINE__, #condition); \
-            exit(EXIT_FAILURE);                                                                 \
-        }                                                                                       \
+#define ASSERT(condition)                                                                          \
+    ({                                                                                             \
+        printf("%s:%i: %s\n", __FILE__, __LINE__, #condition);                                     \
+                                                                                                   \
+        if (!(condition)) {                                                                        \
+            int err = errno;                                                                       \
+            fprintf(stderr, "%s:%i: Assertion failed! (%s), errno = \"%s\"\n", __FILE__, __LINE__, \
+                    #condition, strerror(err));                                                    \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
     })
 #else
-#define ASSERT(condition)                                                                       \
-    ({                                                                                          \
-        if (!(condition)) {                                                                     \
-            fprintf(stderr, "%s:%i: Assertion failed! (%s)\n", __FILE__, __LINE__, #condition); \
-            exit(EXIT_FAILURE);                                                                 \
-        }                                                                                       \
+#define ASSERT(condition)                                                                          \
+    ({                                                                                             \
+        if (!(condition)) {                                                                        \
+            int err = errno;                                                                       \
+            fprintf(stderr, "%s:%i: Assertion failed! (%s), errno = \"%s\"\n", __FILE__, __LINE__, \
+                    #condition, strerror(err));                                                    \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
     })
 #endif
 
@@ -43,10 +50,18 @@ void test_inet();
 void test_ctype();
 void test_fb();
 void test_string();
+void test_pdevs();
+void test_bong();
+void test_signals();
+void test_pthread();
 
 #if __aex__
 void test_aex();
 #endif
+
+void fault_handler(int id) {
+    exit(0x80 | id);
+}
 
 int main(int argc, char* argv[]) {
     if (argc >= 2) {
@@ -59,6 +74,20 @@ int main(int argc, char* argv[]) {
 
             return 0;
         }
+        else if (strcmp(argv[1], "exit") == 0)
+            return 0;
+        else if (strcmp(argv[1], "pagefault") == 0) {
+            struct sigaction act;
+
+            act.sa_handler = fault_handler;
+            act.sa_flags   = 0;
+
+            sigaction(SIGSEGV, &act, NULL);
+
+            *((int*) 0xFFFF800000000000) = 'A';
+            *((int*) 0xFFFF800000000001) = 'E';
+            *((int*) 0xFFFF800000000002) = 'X';
+        }
     }
 
     nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);
@@ -70,6 +99,9 @@ int main(int argc, char* argv[]) {
     test_inet();
     test_ctype();
     test_string();
+    test_pdevs();
+    test_signals();
+    test_pthread();
     // test_fb();
 
 #if __aex__
@@ -81,6 +113,13 @@ int main(int argc, char* argv[]) {
 
     printf("hostname: %s\n", buffer);
     printf("PATH: %s\n", getenv("PATH"));
+
+    printf("test: %s %i %o %x %X %p %c %h% %#x %#X %8x | %0-8x %8s | %+x %+i %+u end\n", "bong",
+           2137, 02137, 0x2137FF, 0x2137FF, &buffer, 65, 0x23ab, 0x23ab, 0x11, 0x11, "abcd", 0xabcd,
+           23, 72);
+
+    snprintf(buffer, 4, "%s", "abcdefgh");
+    printf("%s\n", buffer);
 
     return 0;
 }
@@ -337,6 +376,20 @@ void test_string() {
     memcpy(buffer, "aa", 3);
     ASSERT(strcmp(buffer, "a") > 0);
 
+    // strncmp
+    memcpy(buffer, "aaaaaaaa", 8);
+    ASSERT(strncmp(buffer, "aaaabaaa", 8) < 0);
+    memcpy(buffer, "bbbbbbbb", 8);
+    ASSERT(strncmp(buffer, "bbbbabbb", 8) > 0);
+    memcpy(buffer, "aaaaaaaa", 8);
+    ASSERT(strncmp(buffer, "aaaaaaaa", 8) == 0);
+    memcpy(buffer, "aa", 3);
+    ASSERT(strncmp(buffer, "aaaaaaaa", 8) < 0);
+    memcpy(buffer, "aa", 3);
+    ASSERT(strncmp(buffer, "a", 8) > 0);
+    memcpy(buffer, "asdsadff", 6);
+    ASSERT(strncmp(buffer, "asdsadaa", 6) == 0);
+
     // strcoll
     // soon
 
@@ -347,10 +400,6 @@ void test_string() {
 
     // strncpy
     memcpy(buffer, "aaaaaaaaaaaa", 12);
-    ASSERT(strncpy(buffer, "asdd", 3) == buffer);
-    ASSERT(strlen(buffer) == 12);
-    ASSERT(strncpy(buffer, "asd", 3) == buffer);
-    ASSERT(strlen(buffer) == 12);
     ASSERT(strncpy(buffer, "gd", 3) == buffer);
     ASSERT(strlen(buffer) == 2);
     ASSERT(strncpy(buffer, "g", 3) == buffer);
@@ -394,6 +443,7 @@ void test_string() {
     // strrchr
     memcpy(buffer, "abcdefgd", 9);
     ASSERT(strrchr(buffer, 'd') == &buffer[7]);
+    ASSERT(strrchr(buffer, 'a') == &buffer[0]);
 
     // strspn
     memcpy(buffer, "12321asda!@#asd", 9);
@@ -456,23 +506,63 @@ void test_string() {
     ASSERT(strtok_r(buffer, " ,", &ptr) == NULL);
 }
 
+void test_pdevs() {
+    char buffer[4];
+
+    int null = open("/dev/null", O_RDONLY);
+    ASSERT(read(null, buffer, 4) == 0);
+    close(null);
+
+    FILE* fnull = fopen("/dev/null", "r");
+    ASSERT(fread(buffer, 4, 1, fnull) == 0);
+    ASSERT(getc(fnull) == EOF);
+    fclose(fnull);
+}
+
+void test_bong() {
+    int*  iptr;
+    char* cptr;
+
+#if defined(__i386__)
+    __asm__("pushf\norl $0x40000,(%esp)\npopf");
+#elif defined(__x86_64__)
+    __asm__("pushf\norl $0x40000,(%rsp)\npopf");
+#endif
+
+    /* malloc() always provides memory which is aligned for all fundamental types */
+    cptr = malloc(sizeof(int) + 1);
+
+    /* Increment the pointer by one, making it misaligned */
+    iptr = (int*) ++cptr;
+
+    printf("bong: %p\n", cptr);
+
+    /* Dereference it as an int pointer, causing an unaligned access */
+    *iptr = 42;
+
+    __asm__("ud2");
+
+    /*
+       Following accesses will also result in sigbus error.
+       short *sptr;
+       int    i;
+
+       sptr = (short *)&i;
+       // For all odd value increments, it will result in sigbus.
+       sptr = (short *)(((char *)sptr) + 1);
+       *sptr = 100;
+
+    */
+}
+
 double rando(double min, double max) {
     return min + ((double) rand() / (double) RAND_MAX) * (max - min);
 }
 
 void rect(uint32_t* fb, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t rgb) {
-    if (x >= 1280 || y >= 720)
-        return;
-
-    if (x + width >= 1280)
-        width -= ((x + width) - 1280);
-
-    if (y + height >= 720)
-        height -= ((y + height) - 720);
-
     for (int j = 0; j < height; j++)
         for (int i = 0; i < width; i++)
-            fb[(y + j) * 1280 + x + i] = rgb;
+            fb[((y + j) % 512) * 1280 + ((x + i) % 512)] = rgb;
 }
 
 void test_fb() {
@@ -482,18 +572,20 @@ void test_fb() {
     printf("tty res: %ix%ix%i (%i)\n", ioctl(1, 0x04), ioctl(1, 0x05), ioctl(1, 0x06),
            ioctl(1, 0x07));
 
-    uint32_t* fb = (uint32_t*) mmap(NULL, ioctl(1, 0x07), PROT_READ | PROT_WRITE, 0, 1, 0);
+    uint32_t* fb =
+        (uint32_t*) mmap(NULL, ioctl(1, 0x07), PROT_READ | PROT_WRITE, 0, STDOUT_FILENO, 0);
     if (!fb) {
         perror("mmap failed");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < 1280 * 720; i++)
-        fb[i] = rando(0x000000, 0xFFFFFF);
+    int color[4] = {0x59c4dd, 0x2e7081, 0xc8dadf};
 
-    for (int i = 0; i < 1000; i++)
-        rect(fb, rando(0, 1280), rando(0, 720), rando(0, 1280), rando(0, 720),
-             0x2b2b34 * rando(0.0, 1.0));
+    rect(fb, 0, 0, 512, 512, color[0]);
+
+    for (int i = 0; i < 4000; i++)
+        rect(fb, rando(0, 1280), rando(0, 720), rando(0, 512 / 32), rando(0, 512 / 32),
+             color[i % 3]);
 }
 
 #if __aex__
@@ -503,9 +595,9 @@ void test_aex() {
         memset(mem, 72, 1048576 * 512);
     }*/
 
-    mount("/dev/sda1", "/mnt/", NULL, 0x00, NULL);
+    // mount("/dev/sda1", "/mnt/", NULL, 0x00, NULL);
 
-    mkdir("/mnt/test3", 0);
+    // mkdir("/mnt/test3", 0);
     /*mkdir("/mnt/test3/test4", 0);
     // FILE* aaaa = fopen("/mnt/test3/menel.txt", "r");
     // fclose(aaaa);
@@ -528,5 +620,242 @@ void test_aex() {
 
     // unlink("/mnt/test3/gfd_abcdefghij.txt");
     rename("/mnt/gfd_abcdefghij.txt", "/mnt/test5");*/
+
+    /*while (true) {
+        uint8_t buffer[9] = {};
+
+        int random = open("/dev/random", O_RDONLY);
+        read(random, buffer, 8);
+
+        printf("random says: %i %i %i %i %i %i %i %i\n", buffer[0], buffer[1], buffer[2],
+    buffer[3], buffer[4], buffer[5], buffer[6], buffer[7]);
+
+        nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);
+    }*/
 }
 #endif
+
+void signalbong(int) {
+    printf("it workey\n");
+}
+
+void signalbong_b(int sig, siginfo_t* info, void* ucontext) {
+    ASSERT(sig == SIGUSR1);
+    ASSERT(info->si_signo == SIGUSR1);
+    ASSERT(info->si_value.sival_int == 0x2137);
+    ASSERT(info->si_code == SI_QUEUE);
+}
+
+void test_signals() {
+    struct sigaction act;
+
+    act.sa_handler = signalbong;
+    act.sa_flags   = 0;
+
+    sigaction(SIGUSR1, &act, NULL);
+    kill(getpid(), SIGUSR1);
+
+    if (fork() == 0) {
+        char* const argv[3] = {
+            "utest",
+            "pagefault",
+            NULL,
+        };
+
+        ASSERT(execve("utest", argv, NULL) != -1);
+    }
+    else {
+        int stat;
+        wait(&stat);
+
+        ASSERT(stat == 0x80 | SIGSEGV);
+    }
+
+    sigset_t set;
+    int      sig;
+
+    sigfillset(&set);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+
+    sigaction(SIGUSR1, &act, NULL);
+    kill(getpid(), SIGUSR1);
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    sigwait(&set, &sig);
+
+    ASSERT(sig == SIGUSR1);
+
+    kill(getpid(), SIGUSR1);
+
+    siginfo_t info;
+    ASSERT(sigwaitinfo(&set, &info) == SIGUSR1);
+    ASSERT(info.si_signo == SIGUSR1);
+
+    int ret        = sigtimedwait(&set, &info, (const struct timespec[]){{0, 100000000L}});
+    int errno_pres = errno;
+
+    ASSERT(ret == -1);
+    ASSERT(errno_pres == EAGAIN);
+
+    struct sigaction act_b;
+    union sigval     bong;
+
+    act.sa_sigaction = signalbong_b;
+    act.sa_flags     = SA_SIGINFO;
+    bong.sival_int   = 0x2137;
+
+    ASSERT(sigaction(SIGUSR1, &act, NULL) == 0);
+
+    sigrelse(SIGUSR1);
+    sigqueue(getpid(), SIGUSR1, bong);
+
+    sigfillset(&set);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+    kill(getpid(), SIGUSR1);
+
+    sigset_t pending;
+    sigpending(&pending);
+
+    int signo;
+
+    ASSERT(sigismember(&pending, SIGUSR1));
+    ASSERT(sigwait(&pending, &signo) == 0);
+    ASSERT(signo == SIGUSR1);
+
+    act.sa_handler = signalbong;
+    act.sa_flags   = 0;
+
+    ASSERT(sigaction(SIGUSR1, &act, NULL) == 0);
+
+    sigemptyset(&set);
+    kill(getpid(), SIGUSR1);
+
+    ASSERT(sigsuspend(&set) == -1);
+
+    sigpending(&pending);
+
+    ASSERT(!sigismember(&pending, SIGUSR1));
+    ASSERT(signal(SIGUSR1, SIG_DFL) == signalbong);
+    ASSERT(signal(SIGUSR1, signalbong) == SIG_DFL);
+}
+
+void test_pthread_defcancel();
+void test_pthread_asynccancel();
+void test_pthread_masking();
+void test_pthread_canceltoggle();
+
+void* bong(void* arg) {
+    ASSERT(arg == (void*) 0x7777);
+    return (void*) 0x6666;
+}
+
+void test_pthread() {
+    int oldstate = 0;
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &oldstate);
+    ASSERT(oldstate == PTHREAD_CANCEL_ENABLE);
+
+    int oldtype = 0;
+    pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, &oldtype);
+    ASSERT(oldtype == PTHREAD_CANCEL_DEFERRED);
+
+    pthread_t      thread;
+    pthread_attr_t attr;
+    void*          retval;
+
+    pthread_attr_init(&attr);
+    ASSERT(pthread_create(&thread, &attr, bong, (void*) 0x7777) == 0);
+    ASSERT(pthread_kill(thread, 0) == 0);
+    ASSERT(pthread_join(thread, &retval) == 0);
+
+    ASSERT(retval == (void*) 0x6666);
+
+    test_pthread_defcancel();
+    test_pthread_asynccancel();
+    test_pthread_masking();
+    test_pthread_canceltoggle();
+}
+
+void* test_pthread_defcancel_secondary(void*) {
+    sleep(1000);
+
+    fprintf(stderr, "it brokey\n");
+    exit(EXIT_FAILURE);
+
+    return NULL;
+}
+
+void test_pthread_defcancel() {
+    pthread_t thread;
+
+    ASSERT(pthread_create(&thread, NULL, test_pthread_defcancel_secondary, NULL) == 0);
+    nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);
+
+    ASSERT(pthread_cancel(thread) == 0);
+    ASSERT(pthread_join(thread, NULL) == 0);
+}
+
+void* test_pthread_asynccancel_secondary(void*) {
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+    while (true)
+        ;
+
+    return NULL;
+}
+
+void test_pthread_asynccancel() {
+    pthread_t thread;
+
+    ASSERT(pthread_create(&thread, NULL, test_pthread_asynccancel_secondary, NULL) == 0);
+    nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);
+
+    ASSERT(pthread_cancel(thread) == 0);
+    ASSERT(pthread_join(thread, NULL) == 0);
+}
+
+void test_pthread_masking_handler(int) {
+    fprintf(stderr, "Signal masking is brokey\n");
+    exit(EXIT_FAILURE);
+}
+
+void test_pthread_masking() {
+    struct sigaction act = {};
+
+    act.sa_handler = test_pthread_masking_handler;
+    act.sa_flags   = 0;
+
+    sigaction(SIGUSR1, &act, NULL);
+
+    sigset_t mask;
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
+
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+    pthread_kill(pthread_self(), SIGUSR1);
+}
+
+volatile int canceltoggle_test = 0;
+
+void* test_pthread_canceltoggle_secondary(void*) {
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    pthread_cancel(pthread_self());
+
+    canceltoggle_test = 1;
+
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);
+
+    canceltoggle_test = 2;
+    return NULL;
+}
+
+void test_pthread_canceltoggle() {
+    pthread_t thread;
+
+    ASSERT(pthread_create(&thread, NULL, test_pthread_canceltoggle_secondary, NULL) == 0);
+    ASSERT(pthread_join(thread, NULL) == 0);
+
+    ASSERT(canceltoggle_test == 1);
+}
